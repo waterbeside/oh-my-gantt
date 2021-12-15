2f2cfec6875af5079466926e55f699d2490c3954
waterbeside <waterbeside@163.com>
2021-12-15 09:48:52 +0800

    run lint

--- src/renderer/popTips.ts
+++ src/renderer/popTips.ts
@@ -1,10 +1,10 @@
 import { createElement } from '../utils/helper'
 export function renderPopTips(props: any, ctx: OhMyGantt) {
-  const $arrow = createElement("div", {class: 'omg-pop-tips__arrow'});
-  const $content = createElement("div", {class: 'omg-pop-tips__content'}, props.children);
-  const $popTips = createElement("div", {class: 'omg-pop-tips'}, $arrow, $content);
+  const $arrow = createElement('div', {class: 'omg-pop-tips__arrow'})
+  const $content = createElement('div', {class: 'omg-pop-tips__content'}, props.children)
+  const $popTips = createElement('div', {class: 'omg-pop-tips'}, $arrow, $content)
   if (props.classList && props.classList.length > 0) {
-    $popTips.classList.add(...props.classList);
+    $popTips.classList.add(...props.classList)
   }
   return $popTips
 }
